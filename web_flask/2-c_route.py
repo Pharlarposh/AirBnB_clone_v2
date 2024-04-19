ba19b60999edd92ea4e9d0364f3e40152745d0ef
Pharlarposh <aderomolabolaji@gmail.com>
2024-04-19 14:24:26 +0000

    C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application.
+"""
+
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Route that displays "Hello HBNB!" when accessed.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Route that displays "HBNB" when accessed.
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    Route that displays "C " followed by the value of the text variable.
+    """
+    return "C {}".format(escape(text))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
