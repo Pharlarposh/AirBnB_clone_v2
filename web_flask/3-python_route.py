765de75eab8c4ee3d3401a0809c1bd9bf12f47e7
Pharlarposh <aderomolabolaji@gmail.com>
2024-04-19 16:31:21 +0000

    Python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web application.
+"""
+
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Route that displays "Hello HBNB!" when accessed.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Route that displays "HBNB" when accessed.
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    Route that displays "C " followed by the value of the text variable.
+    """
+    return "C {}".format(escape(text.replace('_', ' ')))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """
+    Route that displays "Python " followed by the value of the text variable.
+    If no text is provided, uses the default value "is cool".
+    """
+    return "Python {}".format(escape(text.replace('_', ' ')))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
